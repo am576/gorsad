8ce0fbe2de44c5622da34b929dbe1e80021cf3fc
am576 <am>
2021-04-04 17:39:51 +0300

    кнопки-ссылки в дашборде админки

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,17 @@
 @extends('admin.master')
+@section('content')
+    <div class="container dashboard">
+        <div class="row">
+            @foreach(config('admin.menu') as $menu_item)
+                <div style="width: 20%; padding: 5px 20px; margin-bottom: 20px;">
+                    <a href="/admin/{{$menu_item['route']}}" class="admin-link">
+                        <span class="mdi mdi-{{$menu_item['icon']}}"></span>
+                        {{$menu_item['title']}}
+                    </a>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+
+@endsection
