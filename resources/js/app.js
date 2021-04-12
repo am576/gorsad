ab2ccc65adaf98ff741b9e31efc20bd60cb4d3bf
am576 <am>
2021-04-12 16:40:23 +0300

    подключен bootstrap-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,15 @@ import CKEditor from '@ckeditor/ckeditor5-vue';
 
 Vue.prototype.$eventBus = new Vue();
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use( CKEditor );
+Vue.use( BootstrapVue )
+Vue.use( IconsPlugin )
 
 Vue.component('category-selector', require('./components/CategorySelector.vue').default);
 Vue.component('attribute-selector', require('./components/AttributeSelector.vue').default);
