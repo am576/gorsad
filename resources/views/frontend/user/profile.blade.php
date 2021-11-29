e77768d30b3a797fb41f0fb0b3c0736105c1ea05
lgorsad <lgorsad>
2021-11-29 12:39:28 +0200

    стилизация дропдауна логина

--- resources/views/frontend/user/profile.blade.php
+++ resources/views/frontend/user/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.app')
 @section('title', 'Gorsad')
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid layout-spacing">
     <user-profile :data="{{$user}}" :tab="{{$tabIndex ?? 0}}"></user-profile>
 </div>
 @endsection
