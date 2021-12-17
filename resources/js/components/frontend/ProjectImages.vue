bea21b18ea8645a81574542b5b8de2ae592dfb5e
lgorsad <lgorsad>
2021-12-17 23:51:37 +0200

    страница проекта

---
+++ resources/js/components/frontend/ProjectImages.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="row slider-wrapper project-images">
+        <splide :options="options" @splide:click="showImage">
+            <splide-slide v-for="image in images" :key="image.path" >
+                <img :src="image.path" alt="">
+            </splide-slide>
+            <template v-slot:controls>
+                <div class="splide__arrows">
+                    <button class="splide__arrow splide__arrow--prev">
+                        <span class="mdi mdi-chevron-left"></span>
+                    </button>
+                    <button class="splide__arrow splide__arrow--next">
+                        <span class="mdi mdi-chevron-right"></span>
+                    </button>
+                </div>
+            </template>
+        </splide>
+        <b-modal id="modal-image" size="lg" hide-footer>
+            <img :src="current_image" alt="" style="width: 100%;">
+        </b-modal>
+    </div>
+</template>
+
+<script>
+    import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+    import { Splide, SplideSlide } from '@splidejs/vue-splide';
+
+    export default {
+        components: {
+            Splide,
+            SplideSlide,
+        },
+        props: {
+            project: {}
+        },
+        data() {
+            return {
+                current_image: '',
+                options: {
+                    type: 'slide',
+                    width: '100%',
+                    height: 300,
+                    gap   : '2rem',
+                    pagination: false,
+                    rewind: true,
+                    perPage: 1,
+                    fixedWidth:300,
+                    cover: true,
+                },
+                images: [
+                    {
+                        path: '/storage/images/projects/project-01.jpg'
+                    },
+                    {
+                        path: '/storage/images/projects/project-02.jpg'
+                    },
+                    {
+                        path: '/storage/images/projects/project-03.jpg'
+                    },
+                    {
+                        path: '/storage/images/projects/project-04.jpg'
+                    },
+                    {
+                        path: '/storage/images/projects/project-05.jpg'
+                    },
+                ]
+            }
+        },
+        methods: {
+            showImage(slide, e) {
+                this.$bvModal.show('modal-image');
+                this.current_image = this.images[e.index].path;
+            },
+            setCurrentImage(image) {
+                this.current_image = image.path;
+            }
+        },
+        created() {
+            this.setCurrentImage(this.images[0]);
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .thumbs {
+        .thumb-link {
+            cursor: pointer;
+            border: 1px solid #1b4b72;
+        }
+        .thumb-image {
+            height: 100px;
+            display: block;
+            background-size: cover;
+            background-position: center;
+        }
+    }
+
+    .preview {
+        img {
+            width: 100%;
+        }
+    }
+
+    .project-images.slider-wrapper {
+        padding: 0 30px;
+        max-width: 1150px !important;
+        .splide__arrow {
+            width: 4rem;
+            height: 4rem;
+            opacity: 1;
+            background: transparent;
+            .mdi {
+                font-size: 100px;
+                color: #8d8d8d;
+            }
+        }
+        .splide__arrow--prev {
+            position: absolute;
+            left: 0;
+            margin-left: -10%;
+        }
+        .splide__arrow--next {
+            position: absolute;
+            right: 0;
+            margin-right: -10%;
+        }
+    }
+</style>
--- resources/views/frontend/projects/project_page.blade.php
+++ resources/views/frontend/projects/project_page.blade.php
@@ -1,66 +1,52 @@
 @extends('frontend.layouts.app')
 @section('title', 'Gorsad')
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid project-page">
     <div class="row justify-content-center">
         <div class="col-md-12 p-0">
-            <home-slider></home-slider>
-            <div class="row" style="margin-top: -80px;">
-                <filter-small :filter_attributes="{{$filter_attributes}}"></filter-small>
-            </div>
-            <div class="pt-3 border-bottom">
-                <div class="home-text">
-                    <div class="container">
-                        <h4>Lorem ipsum dolor</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sin autem est in ea, quod quidam
-                            volunt, nihil impedit hanc nostram comprehensionem summi boni. Ego quoque, inquit, didicerim
-                            libentius si quid attuleris, quam te reprehenderim. Estne, quaeso, inquam, sitienti in bibendo
-                            voluptas? Duo Reges: constructio interrete. Quid est igitur, inquit, quod requiras? Quod autem
-                            ratione actum est, id officium appellamus.
-                        </p>
-                    </div>
-                </div>
-                <div style="margin-top: -25px">
-                    <services-list></services-list>
-                </div>
-                <div class="home-partners">
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="partners-title">Наши клиенты</div>
+           <div class="project-img-main" style="background-image: url('/storage/images/projects/project-05.jpg');">
+               <p class="project-name">Деревья в Центральном парке</p>
+           </div>
+            <div class="row justify-content-center flex-column align-items-center">
+                <div class="project-details text-left">
+                    <p class="project-description">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sin autem est in ea, quod quidam
+                        volunt, nihil impedit hanc nostram comprehensionem summi boni. Ego quoque, inquit, didicerim
+                        libentius si quid attuleris, quam te reprehenderim. Estne, quaeso, inquam, sitienti in bibendo
+                        voluptas? Duo Reges: constructio interrete. Quid est igitur, inquit, quod requiras? Quod autem
+                        ratione actum est, id officium appellamus.
+                    </p>
+                    <div class="row project-params flex-column justify-content-center">
+                        <div class="row">
+                            <div class="col-6">Место</div>
+                            <div class="col-6">ул. Аэропортная</div>
                         </div>
-                        <div class="row pt-5 pb-5" style="justify-content: space-evenly">
-                            <div>
-                                <img src="/storage/images/public/partners/1.png" alt="">
+                        <div class="row">
+                            <div class="col-6">Площадь</div>
+                            <div class="col-6">2000 м<sup>2</sup></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">Заказчик</div>
+                            <div class="col-6">ООО ТВ-Строй</div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">Растения</div>
+                            <div class="col-6">
+                                <a class="plant-tag" href="/products/1" target="_blank">клён остролистный</a>
                             </div>
-                            <div>
-                                <img src="/storage/images/public/partners/2.png" alt="">
-                            </div>
-                            <div>
-                                <img src="/storage/images/public/partners/3.png" alt="">
-                            </div>
-                            <div>
-                                <img src="/storage/images/public/partners/4.png" alt="">
-                            </div>
-                            <div>
-                                <img src="/storage/images/public/partners/5.png" alt="">
-                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">Благоустройство</div>
+                            <div class="col-6">Городской садовник, ООО</div>
                         </div>
                     </div>
                 </div>
-                <div class="home-text2">
-                    <div class="container">
-                        <h4>Lorem ipsum dolor</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sin autem est in ea, quod quidam
-                            volunt, nihil impedit hanc nostram comprehensionem summi boni. Ego quoque, inquit, didicerim
-                            libentius si quid attuleris, quam te reprehenderim. Estne, quaeso, inquam, sitienti in bibendo
-                            voluptas? Duo Reges: constructio interrete. Quid est igitur, inquit, quod requiras? Quod autem
-                            ratione actum est, id officium appellamus.
-                        </p>
-                    </div>
+                <div class="images-wrapper row w-100 justify-content-center" style="background-color: #efefef;">
+                    <project-images></project-images>
                 </div>
+
             </div>
+
         </div>
     </div>
 </div>
