e3b5fa9b80d577ead9206b4395836a0536aba2a2
am <am@am>
2023-07-03 11:10:16 +0300

    make product code not unique

--- database/migrations/2020_05_17_145207_create_products_table.php
+++ database/migrations/2020_05_17_145207_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title',250);
-            $table->string('code',250)->unique();
+            $table->string('code',250);
             $table->text('description');
             $table->smallInteger('category_id')->unsigned();
             $table->mediumInteger('price')->unsigned()->default(0);
